39a3139be9751aeb55c7c8f8c3cfd0f9bea45d96
kent <quentin.sirjean@le-campus-numerique.fr>
2023-10-06 14:10:21 +0200

    added test to get ip and automatically after install

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-vm_name="test-vm"
+vm_name="test-vm1"
 
 if sudo virsh list --all | grep -q "$vm_name"; then
   echo "VM $vm_name already exists. Starting it..."
@@ -16,4 +16,11 @@ sudo virt-install \
 --console pty,target_type=serial \
 --location /home/quentinsirjean/Downloads/debian-12.1.0-amd64-DVD-1.iso \
 --initrd-inject /home/quentinsirjean/test1/preseed.cfg
+
+echo test 
+
+vm_ipv4=$(virsh domifaddr $vm_name | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
+gnome-terminal -- bash -c "ssh root@$vm_ipv4; exec bash"
+
 fi
+
