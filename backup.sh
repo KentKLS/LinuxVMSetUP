2695fb8d945842e927633b75cb3027335e50a191
kent <quentin.sirjean@le-campus-numerique.fr>
2023-10-09 09:10:44 +0200

    finis set up debian,main script + laravel install script

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-vm_name="test-vm1"
+vm_name="test-v1"
 
 if sudo virsh list --all | grep -q "$vm_name"; then
   echo "VM $vm_name already exists. Starting it..."
@@ -15,12 +15,18 @@ sudo virt-install \
 --graphics spice \
 --console pty,target_type=serial \
 --location /home/quentinsirjean/Downloads/debian-12.1.0-amd64-DVD-1.iso \
---initrd-inject /home/quentinsirjean/test1/preseed.cfg
+--initrd-inject /home/quentinsirjean/test1/preseed.cfg \
+--noreboot 
 
-echo test 
+sudo virsh start "$vm_name"
+sleep 30
 
-vm_ipv4=$(virsh domifaddr $vm_name | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
+vm_macAddr=$(virsh dumpxml "$vm_name" | grep "mac address" | awk -F\' '{ print $2}')
+vm_ipv4=$(arp -an | grep "$vm_macAddr" | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
+
+
+scp /home/quentinsirjean/test1/installDep.sh root@$vm_ipv4:/home/
+ssh -o "StrictHostKeyChecking=no" root@$vm_ipv4 "cd /home/ && ./installDep.sh"
 gnome-terminal -- bash -c "ssh root@$vm_ipv4; exec bash"
 
 fi
-
--- laravelInstall.sh
+++ laravelInstall.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/sh
 
 cd /var/www/html/
-composer create-project laravel/laravel laravel
+sudo composer create-project laravel/laravel laravel  
 cd laravel
 php artisan serve
-cat << 'EOF' | sudo tee /etc/apache2/sites-available/laravel.conf  
+sudo cat << 'EOF' | sudo tee /etc/apache2/sites-available/laravel.conf  
 <VirtualHost *:80>
 DocumentRoot /var/www/html/laravel/public/
 ServerName
@@ -15,7 +15,7 @@ ServerName
 	ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
-EOF; \
-a2ensite laravel.conf
-systemctl reload apache2
-systemctl restart apache2
+EOF; 
+sudo a2ensite laravel.conf
+sudo systemctl reload apache2
+sudo systemctl restart apache2
