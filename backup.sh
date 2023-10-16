324253916fd3ec026b12ae0da816280b0b60c3f9
kent <quentin.sirjean@le-campus-numerique.fr>
2023-10-16 11:49:17 +0200

    added comment

--- backup.sh
+++ backup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/sh
 
-vm_name="test-v1"
+#Write a variable that will store the name we wanna give to our VM
+vm_name="test123"
 
+#if else statement that start the VM if it already exist, else it create it with the set specification
 if sudo virsh list --all | grep -q "$vm_name"; then
   echo "VM $vm_name already exists. Starting it..."
   sudo virsh start "$vm_name"
@@ -18,15 +20,22 @@ sudo virt-install \
 --initrd-inject /home/quentinsirjean/test1/preseed.cfg \
 --noreboot 
 
+#start the newly created VM
 sudo virsh start "$vm_name"
+#wait 30sec for the VM to start fully (that could be modyfied but it works for now)
 sleep 30
 
+#First we get the VM mac address to then get the VM ipv4 address 
 vm_macAddr=$(virsh dumpxml "$vm_name" | grep "mac address" | awk -F\' '{ print $2}')
 vm_ipv4=$(arp -an | grep "$vm_macAddr" | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
 
+#We use the ipv4 address we got just above to copy a script into the VM home directory
+scp -o StrictHostKeyChecking=no /home/quentinsirjean/test1/installDep.sh root@$vm_ipv4:/home/
 
-scp /home/quentinsirjean/test1/installDep.sh root@$vm_ipv4:/home/
+#We the connect to the VM and execute said script 
 ssh -o "StrictHostKeyChecking=no" root@$vm_ipv4 "cd /home/ && ./installDep.sh"
-gnome-terminal -- bash -c "ssh root@$vm_ipv4; exec bash"
+
+#We open a new gnome terminal directly connected to the VM so that we can execute whatever we need/want
+gnome-terminal -- bash -c "ssh -o "StrictHostKeyChecking=no" root@$vm_ipv4; exec bash"
 
 fi
--- installDep.sh
+++ installDep.sh
@@ -1,4 +1,5 @@
 
+#Rewrite sources.list file to comment the first line (allowing us to apt install without issue(It's a band aid solution to a problem i didnt really get))
 cat << EOF | tee /etc/apt/sources.list
 #deb cdrom:[Debian GNU/Linux 12.1.0 _Bookworm_ - Official amd64 DVD Binary-1 with firmware 20230722-10:49]/ bookwor>
 
@@ -14,22 +15,33 @@ deb http://cdn.debian.net/debian bookworm-updates main contrib non-free non-free
 deb-src http://cdn.debian.net/debian bookworm-updates main contrib non-free non-free-firmware
 EOF
 
+#Download all the packages needed for the laravel app/database/apache serv
 apt install php php-curl libapache2-mod-php php-cli php-mysql php-gd php-fpm php-mbstring php-bcmath php-json php-xml php-zip php-pdo php-common adminer php-tokenizer mariadb-server -y
 
+#Create a database for the app + user 
 mysql -e "CREATE DATABASE laravel;"
 mysql -e "CREATE USER 'kent'@'localhost' IDENTIFIED BY 'securepassword';"
 mysql -e "GRANT ALL PRIVILEGES ON laravel.* TO 'kent'@'localhost';"
 mysql -e "FLUSH PRIVILEGES;"
+
+#Necessary step to install composer and make it usable
 apt-get install curl -y
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
+
+#Move to where app is supposed to be (apache2 thing) and create new laravel app
 cd /var/www/html
 composer create-project laravel/laravel laravelapp
+
+#Give rights to file for it to work smoothly
 chown -R www-data:www-data /var/www/html/laravelapp
 chmod -R 775 /var/www/html/laravelapp/storage
+
+#move to app root directory and run artisan command 
 cd laravelapp/
 php artisan
 
+#Write apache2 conf file for the app we just created 
 cat << EOF | tee /etc/apache2/sites-available/laravel.conf
 <VirtualHost *:80>
 ServerName example.com
@@ -43,6 +55,7 @@ CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 EOF
 
+#enable: -the conf we just wrote  -apache2 rewrite mod -adminer(mysql GUI) + Restart apache2 so that everything is taken into account
 a2ensite laravel.conf
 a2enmod rewrite
 a2enconf adminer
